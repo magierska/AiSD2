7b5c75bdd45116357b75543edb17f0c4b8207748
magierska <zuzia.magierska@gmail.com>
2017-05-16 15:33:41 +0000

    Dynamic programming

---
+++ asd2-2/asd2-2/ChangeMaking.cs
@@ -0,0 +1,167 @@
+﻿
+using System;
+
+namespace ASD
+{
+
+class ChangeMaking
+    {
+
+    /// <summary>
+    /// Metoda wyznacza rozwiązanie problemu wydawania reszty przy pomocy minimalnej liczby monet
+    /// bez ograniczeń na liczbę monet danego rodzaju
+    /// </summary>
+    /// <param name="amount">Kwota reszty do wydania</param>
+    /// <param name="coins">Dostępne nominały monet</param>
+    /// <param name="change">Liczby monet danego nominału użytych przy wydawaniu reszty</param>
+    /// <returns>Minimalna liczba monet potrzebnych do wydania reszty</returns>
+    /// <remarks>
+    /// coins[i]  - nominał monety i-tego rodzaju
+    /// change[i] - liczba monet i-tego rodzaju (nominału) użyta w rozwiązaniu
+    /// Jeśli dostepnymi monetami nie da się wydać danej kwoty to metochange = null,
+    /// a metoda również zwraca null
+    ///
+    /// Wskazówka/wymaganie:
+    /// Dodatkowa uzyta pamięć powinna (musi) być proporcjonalna do wartości amount ( czyli rzędu o(amount) ) obliczeniowa amount*n (2 amount2*n)
+    /// </remarks>
+    public static int? NoLimitsDynamic(int amount, int[] coins, out int[] change)
+        {
+            int[,] array = new int[amount,2];
+            change = null;
+
+            if (coins.GetLength(0) <= 0)
+                return null;
+
+            for (int j = 0; j < amount; j++)
+            {
+                array[j, 0] = Int32.MaxValue;
+            }
+          
+            for (int j = 0; j < amount; j++)
+            {
+                foreach (int c in coins)
+                {
+                    if (j + 1 == c)
+                    {
+                        array[j, 0] = 1;
+                        array[j, 1] = -1;
+                    }
+                    if (j + 1 + c > amount) continue;
+                    if (array[j, 0] < Int32.MaxValue && array[j + c,0] > array[j,0] + 1)
+                    {
+                        array[j + c, 0] = array[j, 0] + 1;
+                        array[j + c, 1] = j;
+                    }
+                }   
+            }
+            
+            if (amount != 0 && array[amount - 1, 0] == Int32.MaxValue) return null;
+            change = new int[coins.GetLength(0)];
+            for (int i = 0; i < coins.GetLength(0); i++)
+                change[i] = 0;
+
+            if (amount == 0) return 0;
+            int p = amount - 1, s;
+            while (array[p,1] != -1)
+            {
+                s = p - array[p, 1];
+                for (int i = 0; i < coins.GetLength(0); i++)
+                    if (coins[i] == s)
+                    {
+                        change[i]++;
+                        break;
+                    }
+                p -= s;
+            }
+            s = p - array[p, 1];
+            for (int i = 0; i < coins.GetLength(0); i++)
+                if (coins[i] == s)
+                {
+                    change[i]++;
+                    break;
+                }
+            return array[amount-1,0];      // zmienić
+        }
+
+        /// <summary>
+        /// Metoda wyznacza rozwiązanie problemu wydawania reszty przy pomocy minimalnej liczby monet
+        /// z uwzględnieniem ograniczeń na liczbę monet danego rodzaju
+        /// </summary>
+        /// <param name="amount">Kwota reszty do wydania</param>
+        /// <param name="coins">Dostępne nominały monet</param>
+        /// <param name="limits">Liczba dostępnych monet danego nomimału</param>
+        /// <param name="change">Liczby monet danego nominału użytych przy wydawaniu reszty</param>
+        /// <returns>Minimalna liczba monet potrzebnych do wydania reszty</returns>
+        /// <remarks>
+        /// coins[i]  - nominał monety i-tego rodzaju
+        /// limits[i] - dostepna liczba monet i-tego rodzaju (nominału)
+        /// change[i] - liczba monet i-tego rodzaju (nominału) użyta w rozwiązaniu
+        /// Jeśli dostepnymi monetami nie da się wydać danej kwoty to change = null,
+        /// a metoda również zwraca null
+        ///
+        /// Wskazówka/wymaganie:
+        /// Dodatkowa uzyta pamięć powinna (musi) być proporcjonalna do wartości iloczynu amount*(liczba rodzajów monet)
+        /// ( czyli rzędu o(amount*(liczba rodzajów monet)) )
+        /// </remarks>
+        public static int? Dynamic(int amount, int[] coins, int[] limits, out int[] change)
+        {
+            int?[,] arr = new int?[coins.GetLength(0), amount + 1];
+            int?[,] arr2 = new int?[coins.GetLength(0), amount + 1];
+            change = null;
+
+            if (coins.GetLength(0) <= 0)
+                return null;
+
+            for (int i = 0; i < coins.GetLength(0); i++)
+                arr2[i, 0] = arr[i, 0] = 0;
+
+            for (int i = 1; i <= limits[0] && i * coins[0] <= amount; i++)
+                arr2[0, i * coins[0]] = arr[0, i * coins[0]] = i;
+
+            for (int i = 1; i < coins.GetLength(0); i++)
+            {
+                for (int j = 1; j <= amount; j++)
+                {
+                    arr[i, j] = arr[i - 1, j];
+                    if (arr[i,j] != null)
+                        arr2[i, j] = 0;
+                }
+                for (int j = 0; j <= amount; j++)
+                {
+                    if (arr[i - 1, j] != null)
+                    {
+                        for (int k = 1; k <= limits[i] && k * coins[i] + j <= amount; k++)
+                        {
+                            if (arr[i, j + k * coins[i]] == null || arr[i, j + k * coins[i]] > arr[i, j] + k)
+                            {
+                                arr[i, j + k * coins[i]] = arr[i, j] + k;
+                                arr2[i, j + k * coins[i]] = k;
+                            }
+                        }
+                    }
+                }
+            }
+
+            int sum = 0;
+            if (arr[coins.GetLength(0) - 1, amount] != null)
+            {
+                change = new int[coins.GetLength(0)];
+                int t = amount;
+                for (int i = coins.GetLength(0) - 1; i >= 0; i--)
+                {
+                    dynamic p = arr2[i, t];
+                    change[i] = p == null ? 0 : p;
+                    t -= change[i] * coins[i];
+                    sum += change[i]; 
+                }
+            }
+
+            if (amount != 0 && sum == 0)
+                return null;
+
+            return sum;      // zmienić
+        }
+
+    }
+
+}
---
+++ asd2-2/asd2-2/Lab02_Main.cs
@@ -0,0 +1,186 @@
+﻿
+using System;
+
+namespace ASD
+{
+
+    abstract class ChangeMakingTestCase : TestCase
+    {
+
+        protected int amount;
+        protected int[] coins;
+        protected int? expectedRes;
+        protected int[] expectedChange;
+
+        protected int? res;
+        protected int[] change;
+
+        public ChangeMakingTestCase(double timeLimit, Exception expectedException, int a, int[] c, int[] ec) : base(timeLimit, expectedException)
+        {
+            amount = a;
+            coins = c;
+            expectedChange = ec;
+            if (ec == null)
+                expectedRes = null;
+            if (ec != null)
+            {
+                expectedRes = 0;
+                foreach (int cc in ec) expectedRes += cc;
+            }
+        }
+
+        public override void VerifyTestCase(out Result resultCode, out string message)
+        {
+            if (res != expectedRes)
+            {
+                resultCode = Result.BadResult;
+                message = string.Format("incorrect result: {0} (expected: {1})", res == null ? "null" : res.ToString(), expectedRes == null ? "null" : expectedRes.ToString());
+                return;
+            }
+            if ((res == null && change != null) || (res != null && change == null))
+            {
+                resultCode = Result.BadResult;
+                message = $"incorrect change table: {change.ElementsToString()} (expected: {expectedChange.ElementsToString()}";
+                return;
+            }
+            resultCode = Result.Success;
+            message = "OK";
+            if (res == null) return;
+
+            int cc = 0;
+            int a = 0;
+            for (int i = 0; i < coins.Length; ++i)
+            {
+                if (change[i] < 0)
+                {
+                    resultCode = Result.BadResult;
+                    message = $"negative coins number: {change.ElementsToString()}";
+                    return;
+                }
+                cc += change[i];
+                a += coins[i] * change[i];
+            }
+            if (cc != expectedRes)
+            {
+                resultCode = Result.BadResult;
+                message = $"icorrect coins count: {cc}, {change.ElementsToString()} (expected {expectedRes}, {expectedChange.ElementsToString()})";
+                return;
+            }
+            if (a != amount)
+            {
+                resultCode = Result.BadResult;
+                message = $"icorrect change amount: {a}, {change.ElementsToString()} (expected {amount}, {expectedChange.ElementsToString()})";
+                return;
+            }
+        }
+
+    }
+
+    class ChangeMakingLimitsTestCase : ChangeMakingTestCase
+    {
+
+        protected int[] limits;
+
+        public ChangeMakingLimitsTestCase(double timeLimit, Exception expectedException, int a, int[] c, int[] l, int[] ec)
+                   : base(timeLimit, expectedException, a, c, ec)
+        {
+            limits = l;
+        }
+
+        public override void PerformTestCase()
+        {
+            res = ChangeMaking.Dynamic(amount, coins, limits, out change);
+        }
+
+        public override void VerifyTestCase(out Result resultCode, out string message)
+        {
+            base.VerifyTestCase(out resultCode, out message);
+            if (message != "OK") return;
+            resultCode = Result.Success;
+            message = "OK";
+            if (res == null) return;
+            for (int i = 0; i < coins.Length; ++i)
+                if (change[i] > limits[i])
+                {
+                    resultCode = Result.BadResult;
+                    message = $"coins limit break: {change.ElementsToString()}";
+                    return;
+                }
+        }
+
+    }
+
+    class ChangeMakingNoLimitsTestCase : ChangeMakingTestCase
+    {
+
+        public ChangeMakingNoLimitsTestCase(double timeLimit, Exception expectedException, int a, int[] c, int[] ec)
+                   : base(timeLimit, expectedException, a, c, ec)
+        { }
+
+        public override void PerformTestCase()
+        {
+            res = ChangeMaking.NoLimitsDynamic(amount, coins, out change);
+        }
+
+    }
+
+    class Lab02
+    {
+
+        static void Main()
+        {
+            TestSet changeMakingNoLimits = new TestSet();
+            changeMakingNoLimits.TestCases.Add(new ChangeMakingNoLimitsTestCase(5, null, 10, new int[] { 5, 1, 2 }, new int[] { 2, 0, 0 }));
+            changeMakingNoLimits.TestCases.Add(new ChangeMakingNoLimitsTestCase(5, null, 123, new int[] { 1, 2, 5, 10, 20, 50, 100, 200 }, new int[] { 1, 1, 0, 0, 1, 0, 1, 0 }));
+            changeMakingNoLimits.TestCases.Add(new ChangeMakingNoLimitsTestCase(5, null, 123, new int[] { 2, 5, 10, 20, 50, 100, 200 }, new int[] { 4, 1, 0, 1, 0, 1, 0 }));
+            changeMakingNoLimits.TestCases.Add(new ChangeMakingNoLimitsTestCase(5, null, 23, new int[] { 1 }, new int[] { 23 }));
+            changeMakingNoLimits.TestCases.Add(new ChangeMakingNoLimitsTestCase(5, null, 23, new int[] { 2 }, null));
+            changeMakingNoLimits.TestCases.Add(new ChangeMakingNoLimitsTestCase(5, null, 23, new int[] { 50 }, null));
+
+            TestSet changeMakingLimits = new TestSet();
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 10, new int[] { 5, 1, 2 }, new int[] { 1, 20, 10 }, new int[] { 1, 1, 2 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 123, new int[] { 1, 2, 5, 10, 20, 50, 100, 200 },
+                                                                                          new int[] { 99999, 99999, 99999, 99999, 99999, 99999, 99999, 99999 },
+                                                                                          new int[] { 1, 1, 0, 0, 1, 0, 1, 0 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 123, new int[] { 2, 5, 10, 20, 50, 100, 200 },
+                                                                                          new int[] { 99999, 99999, 99999, 99999, 99999, 99999, 99999 },
+                                                                                          new int[] { 4, 1, 1, 0, 0, 1, 0 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 123, new int[] { 1, 2, 5, 10, 20, 50, 100, 200 },
+                                                                                          new int[] { 0, 99999, 99999, 99999, 99999, 99999, 99999, 99999 },
+                                                                                          new int[] { 0, 4, 1, 1, 0, 0, 1, 0 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 123, new int[] { 1, 2, 5, 10, 20, 50, 100, 200 },
+                                                                                          new int[] { 99999, 99999, 3, 4, 3, 0, 0, 99999 },
+                                                                                          new int[] { 0, 4, 3, 4, 3, 0, 0, 0 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 23, new int[] { 1, 2, 5 },
+                                                                                          new int[] { 99999, 99999, 3 },
+                                                                                          new int[] { 0, 4, 3 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 23, new int[] { 5, 1, 2 },
+                                                                                         new int[] { 2, 3, 5 },
+                                                                                         new int[] { 2, 3, 5 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 23, new int[] { 5, 5, 1, 2 },
+                                                                                         new int[] { 3, 2, 3, 3 },
+                                                                                         new int[] { 3, 1, 1, 1 }));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 23, new int[] { 2 }, new int[15], null));
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 23, new int[] { 50 }, new int[1], null));
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 23, new int[] { 1 }, new int[20], null));
+
+            changeMakingLimits.TestCases.Add(new ChangeMakingLimitsTestCase(5, null, 141, new int[] { 2, 137, 65, 35, 30, 9, 123, 81, 71 },
+                                                                                          new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                                                                                          new int[] { 1, 0, 1, 1, 1, 1, 0, 0, 0 }));
+
+            Console.WriteLine("\nChange Making without coins limits");
+            changeMakingNoLimits.PreformTests(verbose: true, checkTimeLimit: false);
+
+            Console.WriteLine("\nChange Making with coins limits");
+            changeMakingLimits.PreformTests(verbose: true, checkTimeLimit: false);
+        }
+    }
+}
