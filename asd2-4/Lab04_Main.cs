d772eab49a76b86e5f00171144c4c56cbce2fc90
magierska <zuzia.magierska@gmail.com>
2017-05-16 15:39:40 +0000

    Dynamic programming

---
+++ asd2-4/Lab04_Main.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Text;
+
+namespace ASD
+{
+    /// <summary>
+    /// Struktura przechowująca wyniki: minimalną i maksymalną prędkość wynikającą z obliczeń.
+    /// </summary>
+    public struct Velocities
+    {
+        public readonly int minVelocity, maxVelocity;
+        public Velocities(int minVelocity, int maxVelocity) { this.minVelocity = minVelocity; this.maxVelocity = maxVelocity; }
+    }
+
+    delegate Velocities Lab04Method(int[] measurements, out bool[] isBraking);
+
+    class VelocityMeasurementsTestCase : TestCase
+    {
+        private readonly int[] measurements;
+        private readonly int minVelocity, maxVelocity;
+        private readonly bool shouldIsBrakingNull;
+        private readonly Lab04Method testedFunction;
+        private Velocities result;
+        private bool[] isBraking;
+
+        public VelocityMeasurementsTestCase(double timeLimit, Lab04Method testedFunction, int[] measurements, int minVelocity, int maxVelocity, bool shouldIsBrakingNull = false) : base(timeLimit, null)
+        {
+            this.measurements = measurements;
+            this.testedFunction = testedFunction;
+            this.minVelocity = minVelocity;
+            this.maxVelocity = maxVelocity;
+            this.shouldIsBrakingNull = shouldIsBrakingNull;
+        }
+
+        public override void PerformTestCase()
+        {
+            result = testedFunction(measurements, out isBraking);
+        }
+
+        public override void VerifyTestCase(out Result resultCode, out string message)
+        {
+            if (minVelocity == result.minVelocity && maxVelocity == result.maxVelocity && IsBrakingArrayCorrect())
+            {
+                resultCode = Result.Success;
+                message = "OK";
+            }
+            else
+            {
+                // W przypadku błędu tworzony jest napis informujący która wartość została źle policzona
+                // Jeżeli obie wartości są błędne dodaję nową linię tak żeby było to czytelniejsze
+                resultCode = Result.BadResult;
+                StringBuilder messageText = new StringBuilder("incorrect ");
+                if (minVelocity != result.minVelocity) messageText.Append("minimum_velocity=" + result.minVelocity + " expected: " + minVelocity + " ");
+                if (minVelocity != result.minVelocity && maxVelocity != result.maxVelocity) messageText.Append("\n\t\t    ");
+                if (maxVelocity != result.maxVelocity) messageText.Append("maximum_velocity=" + result.maxVelocity + " expected: " + maxVelocity + " ");
+                if (!IsBrakingArrayCorrect() && (minVelocity != result.minVelocity || maxVelocity != result.maxVelocity)) messageText.Append("\n\t\t    ");
+                if (!IsBrakingArrayCorrect()) messageText.Append(GetIsBrakingArrayError());
+                message = messageText.ToString();
+            }
+        }
+
+        private bool IsBrakingArrayCorrect()
+        {
+            if (shouldIsBrakingNull && isBraking == null) return true;
+            if (shouldIsBrakingNull && isBraking != null) return false;
+            if (isBraking == null || isBraking.Length != measurements.Length) return false;
+
+            int currentSpeedValue = 0;
+            for (int i = 0; i < measurements.Length; i++)
+            {
+                currentSpeedValue += (isBraking[i] ? -1 : 1) * measurements[i];
+            }
+
+            return (Math.Abs(currentSpeedValue) == this.minVelocity);
+        }
+
+        private string GetIsBrakingArrayError()
+        {
+            StringBuilder errorMessage = new StringBuilder("isBraking array ");
+            if (isBraking == null) errorMessage.Append("should not be null");
+            else if (isBraking != null && shouldIsBrakingNull) errorMessage.Append("should be null");
+            else if (isBraking.Length != measurements.Length) errorMessage.Append("has incorrect length");
+
+            return errorMessage.ToString();
+        }
+    }
+
+    class Lab04_Main
+    {
+        static void Main()
+        {
+            TestSet finalVelocitiesTests = new TestSet();
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, new int[] { 0 }, 0, 0));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, new int[] { 10 }, 10, 10));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, new int[] { 10, 3, 5, 4 }, 2, 22));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, new int[] { 4, 11, 5, 5, 5 }, 0, 30));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, new int[] { 10, 10, 5, 3, 1 }, 1, 29));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, new int[] { 10, 10, 5, 3, 1, 9, 24, 3, 4, 19, 18, 7, 7, 8, 10, 5 }, 1, 143));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, new int[] { 7, 10, 2, 18, 4, 6, 6 }, 1, 53));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, GenerateTestArray(20, 1024), 1, 1101));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, GenerateTestArray(100, 1024), 0, 4650));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, GenerateTestArray(100, 123424), 1, 5337));
+            finalVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.FinalVelocities, GenerateTestArray(1000, 123424), 1, 49209));
+
+            Console.WriteLine("\nFinal velocities tests");
+            finalVelocitiesTests.PreformTests(verbose: true, checkTimeLimit: false);
+
+
+            TestSet journeyVelocitiesTests = new TestSet();
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, new int[] { 10 }, 10, 10, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, new int[] { 10, 1, 1, 1 }, 7, 13, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, new int[] { 10, 3, 5, 4 }, 2, 22, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, new int[] { 4, 11, 5, 5, 5 }, 0, 30, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, new int[] { 10, 10, 5, 3, 1 }, 0, 29, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, new int[] { 5, 5, 10, 23, 45, 2, 1, 23, 9, 0, 8, 4, 1, 24, 86, 5, 6, 100, 353, 4, 5, 67, 32, 45, 23, 34, 56, 32, 23 }, 0, 1031, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, GenerateTestArray(20, 1024), 0, 1101, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, GenerateTestArray(100, 1024), 0, 4650, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, GenerateTestArray(100, 123424), 0, 5337, true));
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, GenerateTestArray(1000, 123424), 0, 49209, true));
+            int[] x = { 2, 2 }, y = GenerateTestArray(1000, 123424), z = new int[x.Length + y.Length];
+            x.CopyTo(z, 0);
+            y.CopyTo(z, x.Length);
+            journeyVelocitiesTests.TestCases.Add(new VelocityMeasurementsTestCase(5, VelocityMeasurements.JourneyVelocities, z, 0, 49213, true));
+
+
+
+            Console.WriteLine("\nJourney velocities tests");
+            journeyVelocitiesTests.PreformTests(verbose: true, checkTimeLimit: false);
+        }
+
+        static int[] GenerateTestArray(int numberOfElements, int seed)
+        {
+            Random r = new Random(seed);
+            int[] testArray = new int[numberOfElements];
+            for (int i = 0; i < numberOfElements; i++) testArray[i] = r.Next(100);
+            return testArray;
+        }
+    }
+}
---
+++ asd2-4/VelocityMeasurements.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASD
+{
+    class VelocityMeasurements
+    {
+        /// <summary>
+        /// Metoda zwraca możliwą minimalną i maksymalną wartość prędkości samochodu w momencie wypadku.
+        /// </summary>
+        /// <param name="measurements">Tablica zawierające wartości pomiarów urządzenia zainstalowanego w aucie Mateusza</param>
+        /// <param name="isBrakingValue">Tablica zwracająca informację dla każdego z pomiarów z tablicy measurements informację bool czy dla sekwencji dającej 
+        /// minimalną prędkość wynikową traktować dany pomiar jako hamujący (true) przy przyspieszający (false)</param>
+        /// <returns>Struktura Velocities z informacjami o najniższej i najwyższej możliwej prędkości w momencie wypadku</returns>
+        /// 
+        /// <remarks>
+        /// Złożoność pamięciowa algorytmu powinna być nie większa niż O(sumy_wartości_pomiarów).
+        /// Złożoność czasowa algorytmu powinna być nie większa niż O(liczby_pomiarów * sumy_wartości_pomiarów).
+        /// </remarks>
+        public static Velocities FinalVelocities(int[] measurements, out bool[] isBrakingValue)
+        {
+            isBrakingValue = new bool[measurements.Length];
+            for (int j = 0; j < measurements.Length; j++)
+                isBrakingValue[j] = false;
+            int max = measurements.Sum();
+            int?[] vel = new int?[max + 1];
+            vel[0] = -1;
+            for (int i = 1; i <= max; i++)
+                vel[i] = null;
+            for (int i = 0; i < measurements.Length; i++)
+            {
+                for (int j = 0; j <= max; j++)
+                {
+                    if (vel[j] != null && vel[j] != i && vel[j + measurements[i]] == null)
+                        vel[j + measurements[i]] = i;
+                    if (j + measurements[i] > max / 2)
+                        break;
+                }
+            }
+            int p = max / 2, min;
+            while (vel[p] == null)
+                p--;
+            min = max - 2 * p;
+            while (vel[p] >= 0)
+            {
+                dynamic t = vel[p];
+                isBrakingValue[t] = true;
+                p = p - measurements[t];
+            }
+            return new Velocities(min, max);
+        }
+
+        /// <summary>
+        /// Metoda zwraca możliwą minimalną i maksymalną wartość prędkości samochodu w trakcie całego okresu trwania podróży.
+        /// </summary>
+        /// <param name="measurements">Tablica zawierające wartości pomiarów urządzenia zainstalowanego w aucie Mateusza</param>
+        /// <param name="isBrakingValue">W tej wersji algorytmu proszę ustawić parametr na null</param>
+        /// <returns>Struktura Velocities z informacjami o najniższej i najwyższej możliwej prędkości na trasie</returns>
+        /// 
+        /// <remarks>
+        /// Złożoność pamięciowa algorytmu powinna być nie większa niż O(sumy_wartości_pomiarów).
+        /// Złożoność czasowa algorytmu powinna być nie większa niż O(liczby_pomiarów * sumy_wartości_pomiarów).
+        /// </remarks>
+        public static Velocities JourneyVelocities(int[] measurements, out bool[] isBrakingValue)
+        {
+            isBrakingValue = null;  // Nie zmieniać !!!
+
+            int max = measurements.Sum(), p;
+            int[] vel = new int[max + 1];
+            for (int i = 0; i <= max; i++)
+                vel[i] = -1;
+            vel[measurements[0]] = 0;
+            for (int i = 1; i < measurements.GetLength(0); i++)
+            {
+                int[] pom = new int[max + 1];
+                for (int j = 0; j <= max; j++)
+                    pom[j] = -1;
+                for (int j = 0; j <= max; j++)
+                    if (vel[j] == i-1)
+                    {
+                        p = Math.Abs(j - measurements[i]);
+                        pom[p] = i;
+                        if (p == 0)
+                            return new Velocities(0,max);
+                        p = j + measurements[i];
+                        pom[p] = i;
+                        if (p == 0)
+                            return new Velocities(0, max);
+                    }
+                for (int j = 0; j <= max; j++)
+                    if (pom[j] >= 0)
+                        vel[j] = pom[j];
+            }
+            int min = 0;
+            while (vel[min] < 0)
+                min++;
+
+            return new Velocities(min, max);
+        }
+    }
+}
